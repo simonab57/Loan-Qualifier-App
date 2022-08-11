7f938cd95884b3bf6af1b66eb0ea053d41d2fe6c
Simon <simon.abennet@spartans.ut.edu>
2022-08-10 21:34:34 -0400

    Here is my code for a loan qualifier app

---
+++ qualifier/utils/fileio.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""Helper functions to load and save CSV data.
+
+This contains a helper function for loading and saving CSV files.
+
+"""
+import csv
+
+
+def load_csv(csvpath):
+    """Reads the CSV file from path provided.
+
+    Args:
+        csvpath (Path): The csv file path.
+
+    Returns:
+        A list of lists that contains the rows of data from the CSV file.
+
+    """
+    with open(csvpath, "r") as csvfile:
+        data = []
+        csvreader = csv.reader(csvfile, delimiter=",")
+
+        # Skip the CSV Header
+        next(csvreader)
+
+        # Read the CSV data
+        for row in csvreader:
+            data.append(row)
+    return data
+
+
+def save_csv(csvpath,data,header=None):
+
+    """Reads the CSV file from path provided.
+
+    Args:
+        csvpath (Path): The csv file path.
+
+    Returns:
+        A list of lists that contains the rows of data from the CSV file.
+
+    """
+    with open(csvpath, "w") as csvfile:
+        data = []
+        csvwriter = csv.writer(csvfile, delimiter=",")
+
+    if header: 
+        csvwriter.writerow(header)
+        
+    csvwriter.writerow(data)
